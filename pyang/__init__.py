079385c5aad8f6adb3588eb150c41c381d81c5cb
Siddharth Sharma <siddharth.sharma@ericsson.com>
2024-07-25 14:44:13 +0200

    Prepare dev pre-release 3
    Signed-off-by: Siddharth Sharma <siddharth.sharma@ericsson.com>

--- pyang/__init__.py
+++ pyang/__init__.py
@@ -1,4 +1,4 @@
 """The pyang library for parsing, validating, and converting YANG modules"""
 
-__version__ = '2.6.2.dev2'
-__date__ = '2024-07-18'
+__version__ = '2.6.2.dev3'
+__date__ = '2024-07-25'
